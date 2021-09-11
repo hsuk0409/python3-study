69d72526094f9475ec955a5e8f98707ed7118fd9
hsuk0409 <hsuk0409@gmail.com>
2021-09-11 13:33:13 +0900

    Creating list and dictionary

---
+++ py-basic-programming/structure.py
@@ -0,0 +1,31 @@
+# list
+students = ["친구1", "친구4", "친구2", "친구3"]
+
+print(students)
+print(len(students))
+students.append('친구5')
+print(students)
+students.remove(students[2])
+print(students)
+students.insert(1, '친구1.5')
+print(students)
+
+for student in students:
+    print(student)
+
+students.sort()
+print(students)
+
+students.sort(reverse=True)
+print(students)
+
+students_clicing = students[1:3]
+print(students_clicing)
+
+students.clear()
+print(students)
+
+# dictionary
+dic = {'name': 'justin'}
+print(dic)
+print(dic.get('name'))
