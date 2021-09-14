a79731317085451ff68ab01dac54869a215e5f29
hsuk0409 <hsuk0409@gmail.com>
2021-09-14 20:07:32 +0900

    파이썬 tdd 연습#1

---
+++ py-basic-programming/tdd/simple_tdd/my_array.py
@@ -0,0 +1,6 @@
+class MyArray(object):
+    def sum(self, size, array_str):
+        numbers = [int(number) for number in array_str.split(' ')]
+        # if size != len(numbers):
+        #     raise Exception('array_str은 size 크기를 가져야합니다.')
+        return sum(numbers)
---
+++ py-basic-programming/tdd/simple_tdd/test_array.py
@@ -0,0 +1,16 @@
+import unittest
+
+from my_array import MyArray
+
+
+class TestArray(unittest.TestCase):
+    def test_sum(self):
+        arr = MyArray()
+        result = arr.sum(5, '1, 2, 3, 4, 10, 12')
+        self.assertEqual(result, 32)
+
+    # def test_sum_raise_exception(self):
+    #     self.assertRaises(Exception, lambda: MyArray.sum(5, '1, 2, 3, 4, 10, 12'))
+
+    # def tearDown(self):
+    #     print('배열에 요소 = {}'.format(self.array))
