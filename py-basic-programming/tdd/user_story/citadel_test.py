b47508247f17fbfa2866ae283add325c8e783bb3
hsuk0409 <hsuk0409@gmail.com>
2021-09-15 19:56:05 +0900

    python tdd study#2

--- py-basic-programming/tdd/user_story/citadel_test.py
+++ py-basic-programming/tdd/user_story/citadel_test.py
@@ -1,19 +1,48 @@
 import unittest
 
+from rick_test import Rick
+
 
 class Citadel(object):
     def __init__(self):
-        self.residents = []
+        self._residents = []
 
-    def get_add_residents(self):
-        return self.residents
+    def get_all_residents(self):
+        return self._residents
+
+    def add_resident(self, resident):
+        self._residents.append(resident)
 
 
-class JustinTests(unittest.TestCase):
+class Morty(object):
+    def __init__(self, universe):
+        self._residents = []
+        self.universe = universe
+
+    def get_all_residents(self):
+        return self._residents
+
+    def add_resident(self, resident):
+        self._residents.append(resident)
+
+
+class CitadelTests(unittest.TestCase):
     def test_get_all_residents(self):
         citadel = Citadel()
-        residents = citadel.get_add_residents()
+        residents = citadel.get_all_residents()
         self.assertCountEqual(residents, [])
+
+    def test_add_residents(self):
+        citadel = Citadel()
+        rick = Rick(111)
+        morty = Morty(111)
+
+        citadel.add_resident(rick)
+        citadel.add_resident(morty)
+        residents = citadel.get_all_residents()
+
+        self.assertEqual(residents[0], rick)
+        self.assertEqual(residents[1], morty)
 
 
 if __name__ == '__main__':
