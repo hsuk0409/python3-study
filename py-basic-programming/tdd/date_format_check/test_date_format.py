75533bff7383d030235336d060a4d7c99bf75814
hsuk0409 <hsuk0409@gmail.com>
2021-09-24 16:10:59 +0900

    python date format test

---
+++ py-basic-programming/tdd/date_format_check/test_date_format.py
@@ -0,0 +1,23 @@
+import unittest
+from datetime import datetime
+
+
+def validate(date_text) -> bool:
+    try:
+        datetime.strptime(date_text, "%y%m%d")
+        return True
+    except ValueError:
+        raise ValueError("올바른 날짜 형식이 아닙니다.")
+
+
+class TestDateFormat(unittest.TestCase):
+    def check_date_format(self):
+        wrong_date = "2021-09-24"
+        right_Date = "20210924"
+
+        self.assertRaises(validate(wrong_date), ValueError)
+        self.assertTrue(validate(right_Date))
+
+
+if __name__ == "__main__":
+    unittest.main()
