517656d4682b7163b421a5bedebba91aedbd9a5a
ChoinSuk <hsuk0409@gmail.com>
2021-10-14 16:02:27 +0900

    변수명 수정

--- py-basic-programming/tdd/date_format_check/test_date_format.py
+++ py-basic-programming/tdd/date_format_check/test_date_format.py
@@ -13,10 +13,10 @@ def validate(date_text) -> bool:
 class TestDateFormat(unittest.TestCase):
     def check_date_format(self):
         wrong_date = "2021-09-24"
-        right_Date = "20210924"
+        right_date = "20210924"
 
         self.assertRaises(validate(wrong_date), ValueError)
-        self.assertTrue(validate(right_Date))
+        self.assertTrue(validate(right_date))
 
 
 if __name__ == "__main__":
